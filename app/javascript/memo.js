228b5dcaf2cd66ece48ae7a04af811b198e1accf
tano-taka <tanowaki.takamasa@gmail.com>
2021-03-09 20:33:24 +0900

    リクエストを処理する

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,13 +1,13 @@
 function post (){
-  const submit = document.getElementById("submit"); 
+  const submit = document.getElementById("submit");
   submit.addEventListener("click", (e) => {
     e.preventDefault();
-    const form = document.getElementById("form")
+    const form = document.getElementById("form");
     const formData = new FormData(form);
     const XHR = new XMLHttpRequest();
-    XHR.open("post", "/posts", true);
+    XHR.open("POST", "/posts", true);
     XHR.responseType = "json";
-    XHR.send(formData)
+    XHR.send(formData);
   });
 }
 
